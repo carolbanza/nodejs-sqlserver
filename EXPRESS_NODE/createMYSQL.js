b6e58ad57973002e509bdf0423a1d3f671a64d06
renatoguara2020 <renatoguara2020@gmail.com>
2025-09-23 20:00:38 -0300

    ok

--- EXPRESS_NODE/createMYSQL.js
+++ EXPRESS_NODE/createMYSQL.js
@@ -24,12 +24,30 @@ let con = mysql.createConnection({
   database: "mydb_node_2025"
 });
 
-con.connect(function(err) {
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   let sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+//   });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   let sql = "INSERT INTO customers (name, address) VALUES ('APPLE COMPUTERS', 'UNITED STATES')";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+//   });
+
+  con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
-  let sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-  con.query(sql, function (err, result) {
+  con.query("SELECT * FROM customers", function (err, result, fields) {
     if (err) throw err;
-    console.log("Table created");
+    console.log(result);
   });
-  });
+});
--- EXPRESS_NODE/express_server.js
+++ EXPRESS_NODE/express_server.js
@@ -1,5 +1,6 @@
 // Import the Express.js module
 const express = require('express');
+const path = require('path');
 
 // Create an Express application instance
 const app = express();
@@ -10,7 +11,8 @@ const port = 3000;
 // Define a route for GET requests to the root URL (/)
 app.get('/', (req, res) => {
   // Send a string as the response
-  res.send('<h1>Hello World from Express!</h1>');
+  //res.send('<h1>Hello World from Express!</h1>');
+  res.sendFile(path.join(__dirname, 'views/index.html'))
 });
 
 // Start the server and listen for incoming requests on the specified port

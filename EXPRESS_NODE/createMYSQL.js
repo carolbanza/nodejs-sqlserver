5178bbb17d20ba62e47597d42084cf47e61befba
renatoguara2020 <renatoguara2020@gmail.com>
2025-09-24 07:50:14 -0300

    ok

--- EXPRESS_NODE/createMYSQL.js
+++ EXPRESS_NODE/createMYSQL.js
@@ -28,15 +28,15 @@ let con = mysql.createConnection({
 
 
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  let sql = "CREATE TABLE clientes (id int NOT NULL AUTO_INCREMENT, name VARCHAR(255), address VARCHAR(255), PRIMARY KEY (id))";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
-  });
-  });
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   let sql = "CREATE TABLE clientes (id int NOT NULL AUTO_INCREMENT, name VARCHAR(255), address VARCHAR(255), PRIMARY KEY (id))";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("Table created");
+//   });
+//   });
 
 // con.connect(function(err) {
 //   if (err) throw err;
@@ -48,10 +48,10 @@ con.connect(function(err) {
 //   });
 //   });
 
-//   con.connect(function(err) {
-//   if (err) throw err;
-//   con.query("SELECT * FROM clientes", function (err, result, fields) {
-//     if (err) throw err;
-//     console.log(result);
-//   });
-// });
+  con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM clientes", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});

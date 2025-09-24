3ce6a4fac1c3af3ba7f4f72bb3dae4f2ab3d1912
renatoguara2020 <renatoguara2020@gmail.com>
2025-09-24 07:47:54 -0300

    ok

--- EXPRESS_NODE/createMYSQL.js
+++ EXPRESS_NODE/createMYSQL.js
@@ -28,30 +28,30 @@ let con = mysql.createConnection({
 
 
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   let sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-//   con.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log("Table created");
-//   });
-//   });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  let sql = "CREATE TABLE clientes (id int NOT NULL AUTO_INCREMENT, name VARCHAR(255), address VARCHAR(255), PRIMARY KEY (id))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  });
 
 // con.connect(function(err) {
 //   if (err) throw err;
 //   console.log("Connected!");
-//   let sql = "INSERT INTO customers (name, address) VALUES ('APPLE COMPUTERS', 'UNITED STATES')";
+//   let sql = "INSERT INTO clientes (name, address) VALUES ('APPLE COMPUTERS', 'UNITED STATES')";
 //   con.query(sql, function (err, result) {
 //     if (err) throw err;
 //     console.log("1 record inserted");
 //   });
 //   });
 
-  con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM customers", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+//   con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM clientes", function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
